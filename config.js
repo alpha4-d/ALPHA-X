b104a3bd144cbe12478b769bdc41f7b68d0d6466
alpha4-d <bdlrhmrafiu@gmail.com>
2024-12-25 20:15:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "hyNmnDJA#QBh_kBjtCoCNDLYbZikBuHWhF3SSlvZLUJ27gwrirag",
+SESSION_ID: process.env.SESSION_ID || "VrFzEQQD#6QouNwFE6UxPbdvhKSa5yiR1B9JLrTzOQccI3tu8dcU",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/9ZpgPk2/FB-IMG-17316065326480038.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello 👋 i am ALPHA-X, I am alive!!!",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
